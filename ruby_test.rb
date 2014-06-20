0b1afa2dc1f95b7e0fb7137b56555e1123515d2e
Ben Brostoff <ben.brostoff@gmail.com>
2014-06-20 13:16:52 -0400

    CSV encoding issue solved

--- ruby_test.rb
+++ ruby_test.rb
@@ -5,5 +5,5 @@ require 'CSV'
 # ic = Iconv.new('UTF-8//IGNORE', 'UTF-8')
 # valid_string = ic.iconv(untrusted_string)
 
-rows = CSV.read('academy_awards_final.csv')
-p rows 
+rows = CSV.read('academy_awards.csv')
+rows.each {|row| puts row.length}
