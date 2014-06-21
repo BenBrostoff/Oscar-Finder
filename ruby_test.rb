9d00276cf6783554996a92f986536a1107866c1a
Ian Shuff <ianvshuff@gmail.com>
2014-06-20 23:40:38 -0400

    Small changes to populating the database.'

--- ruby_test.rb
+++ ruby_test.rb
@@ -5,5 +5,8 @@ require 'CSV'
 # ic = Iconv.new('UTF-8//IGNORE', 'UTF-8')
 # valid_string = ic.iconv(untrusted_string)
 
-rows = CSV.read('academy_awards.csv')
-rows.each {|row| puts row.length}
+rows = CSV.read('academy_awards.csv', :headers => true)
+array = []
+rows.each {|row| array << row.to_hash}
+
+p array 
